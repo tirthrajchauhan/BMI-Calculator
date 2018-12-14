68b04067e70ee9efb0624ec39fa6cb5003b989e8
tirthrajchauhan <tirthraj456@gmail.com>
2018-12-14 15:41:57 -0500

    Final Commit

--- BMI Calculator/TableViewController.swift
+++ BMI Calculator/TableViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
-class TableViewController: UITableViewController {
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var userArray:[Entity] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.fetchData()
+        self.tableView.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,17 +33,36 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+   func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return userArray.count
     }
 
     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let name = userArray[indexPath.row]
+        cell.textLabel!.text = name.name! + "Bmi:-" + name.bmi!
+    return cell
+    }
+    
+    
+    func fetchData(){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+do {userArray = try context.fetch(Entity.fetchRequest())
+     }catch{print(error)}
+        }
+    
+    @IBAction func backbtn(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+        resignFirstResponder()
+    }
     
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
